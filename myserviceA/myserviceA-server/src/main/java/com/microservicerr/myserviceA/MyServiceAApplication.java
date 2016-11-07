51882d11f0ef7259a60806423f8cbf7ed2bd1756
zhiheng.li <zhiheng.li@qishon.com>
2016-11-07 14:31:28 +0800

    修改myserviceA-server的springboot Application启动方式

--- myserviceA/myserviceA-server/src/main/java/com/microservicerr/myserviceA/MyServiceAApplication.java
+++ myserviceA/myserviceA-server/src/main/java/com/microservicerr/myserviceA/MyServiceAApplication.java
@@ -1,23 +1,25 @@
 package com.microservicerr.myserviceA;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.microservice.framework.MySpringApplication;
-
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 /**
  *
  */
+@EnableSwagger2
 @SpringBootApplication
-public class MyServiceAApplication {
+public class MyServiceAApplication implements EmbeddedServletContainerCustomizer {
 
     public static void main(String[] args){
-      MySpringApplication mySpringApplication = new MySpringApplication();
-
-      mySpringApplication.run(args);
+        SpringApplication.run(MyServiceAApplication.class,args);
     }
-
+    public void customize(ConfigurableEmbeddedServletContainer container){
+        container.setPort(8082);
+    }
 
 
 }
